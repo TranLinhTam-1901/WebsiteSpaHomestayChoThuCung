2a9446db95debc05e87250f9591c15bf734338e7
TeikaZ <nguyengiakiet1404@gmail.com>
2025-09-20 14:42:46 +0700

    Them gui mail ve khi dat hang

---
+++ DoAnCoSo Ver 14.0/DoAnCoSo/Helper/EncryptionHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DoAnCoSo.Helpers
+{
+    public static class EncryptionHelper
+    {
+        // Key 32 ký tự (256-bit)
+        private static readonly string Key = "12345678901234567890123456789012";
+        // IV 16 ký tự (128-bit)
+        private static readonly string IV = "1234567890123456";
+
+        public static string Encrypt(string plainText)
+        {
+            if (string.IsNullOrEmpty(plainText))
+                return plainText;
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(Key);
+                aesAlg.IV = Encoding.UTF8.GetBytes(IV);
+                aesAlg.Mode = CipherMode.CBC;
+                aesAlg.Padding = PaddingMode.PKCS7;
+
+                using (var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV))
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (var swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public static string Decrypt(string cipherText)
+        {
+            if (string.IsNullOrEmpty(cipherText))
+                return cipherText;
+
+            try
+            {
+                byte[] buffer = Convert.FromBase64String(cipherText);
+
+                using (Aes aesAlg = Aes.Create())
+                {
+                    aesAlg.Key = Encoding.UTF8.GetBytes(Key);
+                    aesAlg.IV = Encoding.UTF8.GetBytes(IV);
+                    aesAlg.Mode = CipherMode.CBC;
+                    aesAlg.Padding = PaddingMode.PKCS7;
+
+                    using (var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV))
+                    using (var msDecrypt = new MemoryStream(buffer))
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    using (var srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+            catch
+            {
+                // Nếu dữ liệu trong DB chưa mã hóa, trả về raw text
+                return cipherText;
+            }
+        }
+    }
+}
