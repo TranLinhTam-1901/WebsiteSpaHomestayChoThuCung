79f1215cdaa15d9594fbe35501a2b909b7d59d70
TranLinhTam-1901 <tranlinhtamtlt@gmail.com>
2025-12-12 15:26:36 +0700

    fix giao dien

--- DoAnCoSo Ver 14.0/DoAnCoSo/Migrations/20251205075636_Initial.Designer.cs
+++ DoAnCoSo Ver 14.0/DoAnCoSo/Migrations/20251205075636_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DoAnCoSo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20251130030850_Initial")]
+    [Migration("20251205075636_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
